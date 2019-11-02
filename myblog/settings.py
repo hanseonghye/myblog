867151371b97e8e72a0f1143a4566f39bc49fc25
hanseonghye <hans12312@naver.com>
2019-11-03 01:13:35 +0900

    m

--- myblog/settings.py
+++ myblog/settings.py
@@ -94,10 +94,9 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "myblog",
-        "HOST": "postgres://xmfoqdsexldpnm:5bb005993422e5c819fd22e29472dea75c1af8d55b8adfb79b8c54db937f8b84@ec2-174-129-253-104.compute-1.amazonaws.com:5432/d3evoq0qfu0odn",
+        "NAME": "d3evoq0qfu0odn",
+        "HOST": "ec2-174-129-253-104.compute-1.amazonaws.com",
         "PORT": "5432"
     }
 
