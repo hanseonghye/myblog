50ec5701d88bd6433a2716d9630d7ce799e2f32a
hanseonghye <hans12312@naver.com>
2019-11-02 22:51:50 +0900

    heroku

--- myblog/settings.py
+++ myblog/settings.py
@@ -93,10 +93,19 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 #     'default': json.loads(open(KEY_PATH).read())
 # }
 DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myblog",
+        "USER": "hans",
+        "PASSWORD": "hans",
+        "HOST": "localhost",
+        "PORT": "5432"
+    }
 
 }
 
-DATABASES['default'] =  dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
